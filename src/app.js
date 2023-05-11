3f2afbc3b2632629f07d694a9ff497af82e18624
Abanim2 <115773506+Abanim2@users.noreply.github.com>
2023-05-11 12:03:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+// const fs = require("fs");
+// const express = require("express");
+// const app = express();
+
+// // Importing products from products.json file
+// const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+
+// //Middlewares
+// app.use(express.json());
+
+// // GET endpoint for sending the products to client by id
+// //// Endpoint - /api/v1/products/:id
+
+// module.exports = app;
+
+
+
 const fs = require("fs");
 const express = require("express");
 const app = express();
@@ -10,5 +27,19 @@ app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
+app.get("/api/v1/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const product = products.find(p => p.id === id);
+
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "Product fetched successfully",
+    data: { product },
+  });
+});
 
 module.exports = app;
